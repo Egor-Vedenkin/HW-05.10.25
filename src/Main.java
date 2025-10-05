089e62bcbcc3bf7fe39e2ad8d1217e60dbe8d852
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2025-10-05 22:50:44 +0300

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,60 @@ public class Main {
         int[] myArray = {12, 55, 31, 10, 0, 0, 0};
 
         // Задача 2
-        System.out.print(Arrays.toString(arrayInt) + ", ");
-        System.out.print(Arrays.toString(arrayDouble) + ", ");
-        System.out.print(Arrays.toString(myArray));
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (i != arrayInt.length-1) {
+                System.out.print(arrayInt[i] + ", ");
+            } else {
+                System.out.println(arrayInt[i]);
+            }
+        }
 
-        // Задача
+        for (int i = 0; i < arrayDouble.length; i++) {
+            if (i != arrayDouble.length-1) {
+                System.out.print(arrayDouble[i] + ", ");
+            } else {
+                System.out.println(arrayDouble[i]);
+            }
+        }
+
+        for (int i = 0; i < myArray.length; i++) {
+            if (i != myArray.length-1) {
+                System.out.print(myArray[i] + ", ");
+            } else {
+                System.out.println(myArray[i]);
+            }
+        }
+
+        // Задача 3
+        for (int i = arrayInt.length-1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(arrayInt[i] + ", ");
+            } else {
+                System.out.println(arrayInt[i]);
+            }
+        }
+
+        for (int i = arrayDouble.length-1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(arrayDouble[i] + ", ");
+            } else {
+                System.out.println(arrayDouble[i]);
+            }
+        }
+
+        for (int i = myArray.length-1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(myArray[i] + ", ");
+            } else {
+                System.out.println(myArray[i]);
+            }
+        }
+
+        // Задача 4
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] % 2 != 0) {
+                arrayInt[i] += 1;
+            }
+        }
     }
 }
